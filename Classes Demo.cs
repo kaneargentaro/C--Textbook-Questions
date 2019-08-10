805b6e0254bcecc1b7733d8b5befaab4afecb529
Kane Argentaro <kane_argentaro@hotmail.com>
2019-08-10 20:58:26 +1000

    Added Classes demo
    /*
     * C# 3.0: A BEGINNER'S GUIDE EBOOK
     * CHAPTER 4 TOPIC
     * CLASSES
     */

---
+++ Classes Demo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes_Demo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Vehicle minivan = new Vehicle(7, 16, 21);
+            double gallons;
+            int range;
+            int dist = 252;
+
+            range = minivan.Range();
+
+            Console.WriteLine("Minivan can carry {0} passengers with a range of {1}", minivan.Passengers, range);
+
+            gallons = minivan.FuelNeeded(dist);
+
+            Console.WriteLine("To go {0} miles the minivan needs {1} gallons of fuel.", dist, gallons);
+
+        }
+    }
+}
