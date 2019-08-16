65c26cc2519331efb73ea3f057e2df4f7407cdce
Kane Argentaro <kane_argentaro@hotmail.com>
2019-08-17 00:32:27 +1000

    Add files via upload

---
+++ Practice Code C#/Arrays/Array Methods/Array Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Array_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 4, 3, 2, 6, 7, 10};
+
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Array.Sort(array);
+
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Array.Reverse(array);
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Practice Code C#/Strings/CopyStr_Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Copy_String
+{
+    class copystr
+    {
+        static string CopyStr(string first, ref string second)
+        {
+            second = String.Copy(first);
+            Console.WriteLine(first + ", " + second);
+            return second;
+        }
+        static void Main(string[] args)
+        {
+            string str1 = "hello";
+            string str2 = "im good lol";
+
+            Console.WriteLine(str1 + ", " + str2);
+
+            CopyStr(str1, ref str2);
+
+            Console.WriteLine(str1 + ", " + str2);
+
+
+        }
+    }
+}
