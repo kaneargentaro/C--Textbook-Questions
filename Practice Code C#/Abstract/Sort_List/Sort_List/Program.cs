a85718d143f1480b895e48ee6087ae73192358b3
Kane Argentaro <kane_argentaro@hotmail.com>
2019-08-18 21:38:56 +1000

    Add files via upload

---
+++ Practice Code C#/Abstract/Sort_List/Sort_List/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberlist = new List<int>();
+            numberlist.Add(5);
+            numberlist.Add(7);
+            numberlist.Add(10);
+        }
+    }
+}
---
+++ Practice Code C#/Abstract/Sort_List/Sort_List/Sort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_List
+{
+    class Sort<TYPE> where TYPE : IComparable
+    {
+        private List<TYPE> numberlist = new List<TYPE>();
+
+       public void Add(List<int> sort)
+        {
+            numberlist.Add(sort);
+        }
+
+        TYPE Minimum<TYPE>(TYPE first, TYPE second)
+            where TYPE : IComparable<TYPE>
+            {
+                TYPE result = first;
+                if (second.CompareTo(first) < 0)
+                    result = second;
+            Console.WriteLine("Minimum number is {0}", result);
+            return result;
+            }
+
+        TYPE Maximum<TYPE>(TYPE first, TYPE second)
+             where TYPE : IComparable<TYPE>
+        {
+            TYPE result = first;
+            if (first.CompareTo(second) < 0)
+                result = first;
+            Console.WriteLine("Maximum number is {0}", result);
+            return result;
+        }
+    }
+}
