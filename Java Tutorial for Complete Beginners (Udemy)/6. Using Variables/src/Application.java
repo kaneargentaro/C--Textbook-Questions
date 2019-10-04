d03c3b28c9d05f45e4f6596be3197034c9e0c29e
Kane Argentaro <kane_argentaro@hotmail.com>
2019-10-04 18:19:42 +1000

    Add files via upload

---
+++ Java Tutorial for Complete Beginners (Udemy)/6. Using Variables/src/Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 9875;
+		double myDouble = 7.2184;
+		float myFloat = 324.5f;
+		char myChar = 'y';
+		boolean myBoolean = true;
+		byte myByte = 127;
+		
+		System.out.println("The value of myNumber is: " + myNumber);
+		System.out.println("The value of myShort is: " + myShort);
+		System.out.println("The value of myLong is: " + myLong);
+		System.out.println("The value of myDouble is: " + myDouble);
+		System.out.println("The value of myFloat is: " + myFloat);
+		System.out.println("The value of myChar is: " + myChar);
+		System.out.println("The value of myBoolean is: " + myBoolean);
+		System.out.println("The value of myByte is: " + myByte);
+		
+	}
+
+}
